369d7f973e4ad0a07d9d9f64754bd136d77791ba
root <root@vps691472.ovh.net>
2021-11-09 22:18:46 +0100

    log fonctionnels

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const local = require('./local.js');
-const {Client, Intents} = require('discord.js');
+const _local = require('./local.js');
+const {Client, Intents, MessageEmbed} = require('discord.js');
 const util = require('util')
 
 let GLOBAL = {}
-
+GLOBAL.local = _local;
 
 let {channelTree} = require("./channelTree.js")
 let {messageDelete} = require("./messageDelete.js")
 let {messageSend} = require("./messageSend.js")
 
-
-GLOBAL.local = local;
+var mysql = require('mysql');
+GLOBAL.con = mysql.createConnection({
+	host: GLOBAL.local.db.HOST,
+	user: GLOBAL.local.db.USER,
+	password: GLOBAL.local.db.PSWD,
+	database: GLOBAL.local.db.DDB,
+	charset : 'utf8mb4_bin'
+});
+try{
+	GLOBAL.con.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected to database");
+		return 1;
+	});
+}
+catch(err){
+	console.log.error('Database connexion failed.')
+	return 0;
+}
 
 GLOBAL.bot = new Client({intents : [
 	Intents.FLAGS.GUILDS, 
@@ -22,7 +39,7 @@ GLOBAL.bot = new Client({intents : [
 	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS  
 ],  partials: ["CHANNEL"]});
 
-GLOBAL.bot.login(local.bot.TOKEN)
+GLOBAL.bot.login(GLOBAL.local.bot.TOKEN)
 
 GLOBAL.bot.on('ready', () => {
 	console.log("Bot connecté");
@@ -63,7 +80,273 @@ GLOBAL.bot.on('ready', () => {
 	// })
 })
 
-.on('messageCreate', (message) =>{	
+.on('raw', data => {
+	if(data.t && data.t == 'MESSAGE_DELETE') {
+		guildSaveId = "907674445437624331";
+		let localLog = (e) => {
+			console.log(e)
+		}
+		
+		GLOBAL.con.query("SELECT * FROM `SaveBotMessages` AS M JOIN `SaveBotChannels` AS C ON C.`originId` = M.`channelId` WHERE `messageId` = " + GLOBAL.con.escape(data.d.id) +";", 
+		(err, res, fields) => {
+			if (err) {
+				console.log(err)
+			}
+			else if (typeof(res[0]) != "undefined") {
+				console.log(data)
+				res = res[0];
+				console.log(res)
+				GLOBAL.bot.channels.fetch(res.destId).then(chan => {
+					chan.messages.fetch(res.saveId).then(msg => {
+						
+						
+						// console.log(msg);
+						// console.log("---")
+						// console.log(typeof(msg.embeds))
+						// console.log(msg.embeds[0])
+						// console.log(msg.embeds.length)
+						
+						new Promise( (resolve) => {
+							let i = 0;
+							let embedArray = []
+							msg.embeds.forEach(emb => {
+								i++;
+								console.log(emb)
+								embedArray.push(emb);
+								if (i == msg.embeds.length) {
+									resolve(embedArray)
+								}
+							})
+						})
+						.then(embedArray => {
+							let embed = embedArray[embedArray.length - 1]
+							
+							let mtn = new Date();
+							
+							if (embed.fields.length == 24) {
+								embed = new MessageEmbed()
+								.setTimestamp(Date.now())
+								.setDescription("Embed n°" + embedArray.length)
+								.setFooter("last change")
+								embedArray.push(embed);
+							}
+							embed.setTimestamp(Date.now())
+								.setFooter("last change")
+								.setColor([255, 100, 100])
+								.addField("Supprimé à`"+ Date.now() +"` ("+ mtn +")", "---")
+							
+							msg.edit({
+								embeds : embedArray
+							}).catch(e => {
+								console.log("Couldn't edit message")
+								console.log(e)
+							})
+						})
+					})
+				})
+			} else {
+				console.log(res)
+				console.log("Message unknown in db")
+			}
+		});
+	}
+	else if(data.t && data.t == 'MESSAGE_UPDATE' && data.d.author.id != GLOBAL.local.bot.BOTID) {
+		
+		guildSaveId = "907674445437624331";
+		let localLog = (e) => {
+			console.log(e)
+		}
+		
+		GLOBAL.con.query("SELECT * FROM `SaveBotMessages` AS M JOIN `SaveBotChannels` AS C ON C.`originId` = M.`channelId` WHERE `messageId` = " + GLOBAL.con.escape(data.d.id) +";", 
+		(err, res, fields) => {
+			if (err) {
+				console.log(err)
+			}
+			else if (typeof(res[0]) != "undefined") {
+				// console.log(data)
+				res = res[0];
+				console.log(res)
+				GLOBAL.bot.channels.fetch(res.destId).then(chan => {
+					GLOBAL.bot.channels.fetch(res.originId).then(chanOrigin => {
+						chan.messages.fetch(res.saveId).then(msg => {
+							chanOrigin.messages.fetch(res.messageId).then(msgOrigin => {
+								new Promise( (resolve) => {
+									let i = 0;
+									let embedArray = []
+									msg.embeds.forEach(emb => {
+										i++;
+										console.log(emb)
+										embedArray.push(emb);
+										if (i == msg.embeds.length) {
+											resolve(embedArray)
+										}
+									})
+								})
+								.then(embedArray => {
+									let embed = embedArray[embedArray.length - 1]
+									
+									let mtn = new Date();
+									
+									if (embed.fields.length >= 23) {
+										embed = new MessageEmbed()
+										.setTimestamp(Date.now())
+										.setDescription("Embed n°" + embedArray.length)
+										.setFooter("last change")
+										embedArray.push(embed);
+									}
+									embed.setTimestamp(Date.now())
+										.setFooter("last change")
+										.setColor([100, 100, 255])
+										.addField("Edité à `"+ Date.now() +"` ("+ mtn +")", msgOrigin.content.substr(0, 1023))
+									
+									if (msgOrigin.content.length > 1023)
+										embed.addField("Suite message:", msgOrigin.content.substr(1024))
+									
+									msg.edit({
+										embeds : embedArray
+									}).catch(e => {
+										console.log("Couldn't edit message")
+										console.log(e)
+									})
+								})
+							})
+						})
+					})
+				})
+			} else {
+				console.log(res)
+				console.log("Message unknown in db")
+			}
+		});
+	}
+})
+.on('messageCreate', (message) =>{
+	
+	if (message.guildId === GLOBAL.local.bot.GUILDID) {
+		guildSaveId = "907674445437624331";
+		
+		let localLog = (e) => {
+			//console.log(e)
+		}
+		
+		let archiveMessage = (msg, channelId) => {
+			GLOBAL.bot.channels.fetch(channelId).then(chan => {
+				localLog("Channel fetche")
+				GLOBAL.guild.members.fetch(msg.author.id).then(member => {
+					let mtn = new Date();
+					let embed = new MessageEmbed()
+						.setAuthor((member.nickname ? member.nickname + '(' + member.user.username + ')' : member.user.username), member.displayAvatarURL())
+						.setFooter("last change")
+						.setTimestamp(msg.createdTimestamp)
+						.setDescription("Embed n°0")
+						.setColor([100, 255, 100])
+						.addField("Posté à `"+ msg.createdTimestamp +"` ("+ mtn + ")", message.content.substr(0, 1023))
+					
+					if (message.content.length > 1023)
+						embed.addField("Suite message:", message.content.substr(1024))
+						
+					if (msg.deleted)
+						embed.setColor([255, 100, 100])
+					else if (msg.editedAt != null)
+						embed.setColor([100, 100, 255])
+					
+					chan.send({
+						embeds : [embed],
+						// attachments : msg.attachments,
+						stickers : msg.stickers,
+						files : msg.attachments
+					})
+					.then(msgSent => {
+						localLog("Message envoye")
+						GLOBAL.con.query("INSERT INTO `SaveBotMessages`(`messageId`, `channelId`, `saveId`) VALUES ("+ GLOBAL.con.escape(msg.id) +", "+  GLOBAL.con.escape(msg.channelId) +", "+  GLOBAL.con.escape(msgSent.id) +")")
+					})
+
+				})
+			}).catch(e => {
+				console.log("Error while fetching channel for log: ")
+				console.log(res)
+				console.log(e)
+			})
+		}
+		
+		GLOBAL.con.query("SELECT * FROM `SaveBotChannels` WHERE `originId` = " + GLOBAL.con.escape(message.channelId) +";", 
+		(err, res, fields) => {
+			if (err) {
+				console.log(err)
+				return;
+			}
+			if (typeof(res[0]) != "undefined")
+			{
+				localLog("Channel trouve")
+				archiveMessage(message, res[0].destId);
+			}
+			else {
+				localLog("Le channel n'existe pas")
+				GLOBAL.bot.channels.fetch(message.channelId).then(channel => {
+					localLog("On fetch le channel d'origine")
+					GLOBAL.bot.guilds.fetch(guildSaveId).then(guildSave => {
+						localLog("On fetch la guild de save")
+						new Promise((resolve) => {
+							if (channel.parentId == null) {
+								localLog("Pas de parent")
+								resolve(null)
+							}
+							else {
+								GLOBAL.con.query("SELECT * FROM `SaveBotChannels` WHERE `originId` = " + GLOBAL.con.escape(channel.parentId) +";", 
+								(err, res, fields) => {
+									if (err)
+										console.log(err)
+									else if (typeof(res[0]) == "undefined") {
+										localLog("Parent pas dans la BDD")
+										GLOBAL.bot.channels.fetch(channel.parentId).then(channelParentOrigin => {
+											localLog("On fetch le parent")
+											guildSave.channels.create(channelParentOrigin.name, {
+												type : channelParentOrigin.type, 
+												position : channelParentOrigin.rawPosition, 
+												topic: channelParentOrigin.topic
+											})
+											.then(chanParentDest => {
+												localLog("On creer le parent")
+												GLOBAL.con.query("INSERT INTO `SaveBotChannels`(`originId`, `destId`) VALUES ("+ GLOBAL.con.escape(channelParentOrigin.id) +", "+  GLOBAL.con.escape(chanParentDest.id) +")", (err) => {
+													localLog("On insert le parent dans la BDD")
+													if (err)
+														console.log(err)
+													else
+														resolve(chanParentDest.id)
+												})
+											})
+										})
+									}
+									else {
+										localLog("Parent EST dans la bdd, on resolve")
+										resolve(res[0].destId)
+									}
+								});
+							}
+						})
+						.then(chanParentDestId => {
+							guildSave.channels.create(channel.name, {
+								type : channel.type,
+								topic : channel.topic,
+								parent : chanParentDestId,
+								position : channel.rawPosition
+							}).then(chan => {
+								
+								GLOBAL.con.query("INSERT INTO `SaveBotChannels`(`originId`, `destId`) VALUES ("+ GLOBAL.con.escape(channel.id) +", "+  GLOBAL.con.escape(chan.id) +")", (err) => {
+									if (err)
+										console.log(err)
+									else {
+										archiveMessage(message, chan.id);
+									}
+								})
+							})
+						});
+					})
+				})
+			}
+		})
+	}
+	
 	if (message.content.startsWith("???ping")) {
 		message.author.send("Emergency pong!");
 	}
@@ -99,20 +382,10 @@ GLOBAL.bot.on('ready', () => {
 				}
 				break;
 			}
-			case 'say': {
-				if (message.author.id == "292808250779369482")
-				{
-					let message_trim = message.content.substring((GLOBAL.local.prefix + 'say ').length);
-					message.delete()
-					.then(_ => {
-						message.channel.send(message_trim)
-					});
-				}
-				break;
-			}
+			case 'say' :
 			case 'send' : {
 				if (message.author.id == "292808250779369482") {
-					let messageTrim = message.content.substring((GLOBAL.local.prefix + 'send ').length);
+					let messageTrim = message.content.substring((GLOBAL.local.prefix + command + ' ').length);
 					message.delete();
 					messageSend(GLOBAL, message, message.channelId, messageTrim)
 					.then(res => {
@@ -125,11 +398,11 @@ GLOBAL.bot.on('ready', () => {
 				}
 				break;
 			}
+			case 'ssay' :
 			case 'ssend' : {
 				if (message.author.id == "292808250779369482") {
-					
 					let channelID = args[0];
-					let messageTrim = message.content.substring((GLOBAL.local.prefix + 'send ' + args[0] + " ").length);
+					let messageTrim = message.content.substring((GLOBAL.local.prefix + command + ' ' + args[0] + " ").length);
 					
 					messageSend(GLOBAL, message, channelID, messageTrim)
 					.then(res => {
