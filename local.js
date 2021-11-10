8b47e372940da7f43d4e9320570ccadfc445d2b5
root <root@vps691472.ovh.net>
2021-11-11 00:01:38 +0100

    update local.js

--- local.js
+++ local.js
@@ -1,7 +1,15 @@
 exports.bot = {
 	TOKEN : "aaaaaaaaaaaaaaaaaaaaaaaa.bbbbbb.ccccccccccccccccccccccccccc",
-	BOTID : "111111111111111111",
-	GUILDID : "000000000000000000"
+	BOTID : "000000000000000000",
+	GUILDID : "111111111111111111",
+	GUILDSAVEID : "222222222222222222"
 };
 
-exports.prefix = "?"; 
+exports.db = {
+        HOST : "localhost",
+        USER : "USERNAME",
+        PSWD : "PASSWORD",
+        DDB  : "UserDB"
+};
+
+exports.prefix = "?";
